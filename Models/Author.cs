6288d540478151847e6d08ada4edc8ffbbe5b771
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2025-03-17 03:42:36 +0500

    Add EF Core and Swagger support with new models
    Updated project file to include Entity Framework Core and Swashbuckle.AspNetCore packages. Added AppDbContext class to manage database context with Authors and Books DbSets. Introduced Author and Book model classes to establish relationships between authors and their books.

---
+++ Models/Author.cs
@@ -0,0 +1,11 @@
+﻿namespace ExplictLoadingInAspNetCoreWebapi.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // Navigation property
+        public List<Book> Books { get; set; } = new List<Book>();
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace ExplictLoadingInAspNetCoreWebapi.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+
+        // Navigation property
+        public Author Author { get; set; }
+    }
+}
