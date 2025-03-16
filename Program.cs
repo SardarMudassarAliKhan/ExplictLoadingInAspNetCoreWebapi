8bde719e0228b87bbfd7e64e46b8e35535ecb45d
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2025-03-17 03:45:22 +0500

    Add Entity Framework Core and configure database
    - Included EF Core packages in the project file for design, SQL Server, and tools.
    - Updated `Program.cs` to set up `AppDbContext` with a SQL Server connection.
    - Added connection string for `LibraryDb` in `appsettings.json`.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 
+using ExplictLoadingInAspNetCoreWebapi.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace ExplictLoadingInAspNetCoreWebapi
 {
     public class Program
@@ -8,6 +11,8 @@ namespace ExplictLoadingInAspNetCoreWebapi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
